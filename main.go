201ecf1ad995dfab3ff5cac5352878ce3b1ca232
Takeshi Kameyama <tkame123@gmail.com>
2020-02-05 17:02:27 +0900

    fixed

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func main() {
@@ -17,6 +18,7 @@ func main() {
 func handleParams(w http.ResponseWriter, r *http.Request) {
 	// クエリパラメータ取得してみる
 	fmt.Fprintf(w, "クエリ：%s\n", r.URL.RawQuery)
+	fmt.Fprintf(os.Stdout, "クエリ：%s\n", r.URL.RawQuery)
 
 	// Bodyデータを扱う場合には、事前にパースを行う
 	r.ParseForm()
@@ -24,9 +26,11 @@ func handleParams(w http.ResponseWriter, r *http.Request) {
 	// Formデータを取得.
 	form := r.PostForm
 	fmt.Fprintf(w, "フォーム(Post Only)：\n%v\n", form)
+	fmt.Fprintf(os.Stdout, "フォーム(Post Only)：\n%v\n", form)
 
 	// または、クエリパラメータも含めて全部.
 	params := r.Form
 	fmt.Fprintf(w, "フォーム2(Post and Query)：\n%v\n", params)
+	fmt.Fprintf(os.Stdout, "フォーム2(Post and Query)：\n%v\n", params)
 
 }
